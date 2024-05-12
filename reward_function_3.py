34b2cac5021adbc9fee3fca150a7c6206ee6cf7e
Raju Rayavarapu <raju.rayavarapu@gmail.com>
2024-05-11 21:33:45 -0400

    Update reward_function_3.py
    Testing the step_reward

--- reward_function_3.py
+++ reward_function_3.py
@@ -7,41 +7,22 @@ def reward_function(params):
     distance_from_center = params['distance_from_center']
     track_width = params['track_width']
     steering = abs(params['steering_angle']) # Only need the absolute steering angle
-
-    # Calculate 3 marks that are farther and father away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
-
-    # Give higher reward if the car is closer to center line and vice versa
-    distance_reward = 1- (distance_from_center/track_width*0.5)**0.4
-
-    # Wheels on Track Penalty
-    SPEED_THRESHOLD = 1.0
-    if not all_wheels_on_track:
-           track_reward = 1e-3
-       elif speed < SPEED_THRESHOLD:
-           track_reward = 0.1
-       else:
-           track_reward = 10.0
-
-    #Continuous Speed Reward
-    speed_reward = 1-(speed_diff/max_speed_diff)**).5
-
-    # Steering penality threshold, change the number based on your action space setting    ABS_STEERING_THRESHOLD = 15
-
-    # Penalize reward if the car is steering too much
-    if steering > ABS_STEERING_THRESHOLD:
-        abs_reward *= 0.8
-
-    reward =  distance_reward + track_reward + speed_reward + abs_reward
-
-
     steps = params['steps']
     progress = params['progress']
+
+    # Give higher reward if the car is closer to center line and vice versa using a continuous reward. 
+    distance_reward = 1- (distance_from_center/track_width*0.5)**0.4
+
+    # Total num of steps we want the car to finish the lap, it will vary depends on the track length
     TOTAL_NUM_STEPS = 300
 
-    if (steps % 100) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100:
-         steps_reward +=10.0
+    # Initialize the reward with typical value
+    reward = 1.0
+
+    # Give additional reward if the car pass every 100 steps faster than expected
+    if (steps % 100) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100 :
+        step_reward += 10.0
+    
+    reward = (distance_reward + step_reward)
 
     return float(reward)
