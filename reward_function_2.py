2dae2f6e32469a33542670c39538d6d0dfb1f33f
Raju Rayavarapu <raju.rayavarapu@gmail.com>
2024-05-11 21:17:45 -0400

    Update reward_function_2.py
    Added back the default too much steering component to reduce wiggles

--- reward_function_2.py
+++ reward_function_2.py
@@ -11,6 +11,13 @@ def reward_function(params):
     # Give higher reward if the car is closer to center line and vice versa using a continuous reward. 
     distance_reward = 1- (distance_from_center/track_width*0.5)**0.4
 
+    # Steering penality threshold, change the number based on your action space setting
+    ABS_STEERING_THRESHOLD = 15 
+
+    # Penalize reward if the car is steering too much
+    if abs_steering > ABS_STEERING_THRESHOLD:
+        distance_reward*= 0.8
+    
     reward = (distance_reward)
 
     return float(reward)
